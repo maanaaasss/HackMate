fe78a64f3b7fb6268b08a1ce48454d3747c4504a
Manas <manasgedam26@gmail.com>
2026-04-21 12:08:40 +0530

    linter v8

--- src/app/(organiser)/organiser/setup/page.tsx
+++ src/app/(organiser)/organiser/setup/page.tsx
@@ -1,5 +1,7 @@
 import HackathonWizard from '@/components/organiser/HackathonWizard'
 
+export const dynamic = 'force-dynamic'
+
 export default function HackathonSetupPage() {
   return <HackathonWizard />
 }
